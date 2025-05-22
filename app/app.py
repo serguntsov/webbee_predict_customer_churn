4a0dcafb883df33c938fa7a98b8e41a0de634e56
serguntsov <romanmc2203@mail.ru>
2025-05-22 12:57:14 +0300

    создание приложения

---
+++ app/app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import os.path
+
+# Загрузка модели
+MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'model', 'GBC_model.pkl'))
+
+if os.path.exists(MODEL_PATH):
+    model = joblib.load(MODEL_PATH)
+else:
+    st.error("Возникли трудности с загрузкой модели")
+    st.stop()
+
+st.title("Предсказание на основе CSV")
+ 
+uploaded_file = st.file_uploader("Загрузите ваш CSV-файл с информацией клиентов банка", type="csv")
+
+if uploaded_file is not None:
+    try:
+        df = pd.read_csv(uploaded_file)
+        # Небольшая подготовка данных
+        df = (df.set_index('id')
+                .drop(['CustomerId', 'Surname'], axis=1))
+
+        st.write("Пример входных данных:")
+        st.dataframe(df.head())
+
+        y_pred = model.predict(df)
+        df["Exited"] = y_pred
+
+        st.success("Предсказания выполнены!")
+        st.write("Предсказанные данные:")
+        st.dataframe(df.head())
+
+        # Сохранение в память
+        csv_out = df.to_csv(index=False).encode("utf-8")
+        st.download_button(
+            label="📥 Скачать результат (CSV)",
+            data=csv_out,
+            file_name="predictions.csv",
+            mime="text/csv"
+        )
+
+    except Exception as e:
+        st.error(f"Возникла ошибка при обработке файла: {e}")
